02238d6622edc530c8477abf307cff5dffec6fb2
Oscar Palmer <opalmer@me.com>
2026-03-04 10:49:37 +0100

    0.41.1 - fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './global';
 
-export {delay} from '@oscarpalmer/atoms/promise';
+export {delay} from '../node_modules/@oscarpalmer/atoms/src/promise/delay';
+export type {PromiseOptions} from '../node_modules/@oscarpalmer/atoms/src/promise/models';
 export {isRepeated, isTimer, isWaited, isWhen} from './is';
 export * from './repeat';
 export type {Timer} from './timer';
